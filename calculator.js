10c20b7484cdf2db5bfed4ff5175b06f9115e0be
Agata-88 <118485184+Agata-88@users.noreply.github.com>
2024-01-06 17:27:11 +0000

    decimal point addition

--- calculator.js
+++ calculator.js
@@ -4,9 +4,23 @@ function clearDisplay() {
     document.getElementById('display').value = '';
   }
   
-  function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+  function appendToDisplayNumber(value) {
+    document.getElementById('display').value +=value;
   }
+
+  function appendToDisplay(value) {
+    var displayElement = document.getElementById('display');
+    var currentValue = displayElement.value;
+
+    // Check if the value is not already present to avoicd character duplication
+    if (!currentValue.length === 0 || currentValue.slice(-1) !==value) {
+      displayElement.value += value
+    }
+  }
+
+  //function removeLastCharacter(value) {
+ //   document.getElementById('display').value -=value;
+ // }
   
   function calculateResult() {
     try {
