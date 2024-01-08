962c2c79e8cc45f059cef7578940e41a525456ca
Agata Sierzega <AgataSierzega@gmail.com>
2024-01-08 20:57:01 +0000

    added testing for decimal numbers

--- cypress/e2e/division.cy.js
+++ cypress/e2e/division.cy.js
@@ -1,6 +1,6 @@
 describe('Division test', () => {
   it('should correctly divide two numbers', () => {
-    cy.visit('http://127.0.0.1:5500/index.html')
+    cy.visit('../../index.html')
     
     const divide = (numerator, denominator, expectedValue) => {
          // Helper function to input a large number using buttons
@@ -26,31 +26,39 @@ describe('Division test', () => {
   
     // Test cases
     const testCases= [
-      // Testing positive integers, including very large numbers
+      // Testing positive integers, including very large numbers and floats
       {numerator: "6", denominator: "2", expectedValue: "3"},
       {numerator: "10", denominator: "5", expectedValue: "2"},
       {numerator: "150", denominator: "3", expectedValue: "50"},
+      {numerator: "1.5", denominator: "3", expectedValue: "0.5"},
+      {numerator: "15045698.215478", denominator: "15.9658", expectedValue: "942370.4553156121"},
       {numerator: "1500000000000000", denominator: "399999999999999", expectedValue: "3.7500000000000093"},
       {numerator: "15597562143664", denominator: "3996579132645", expectedValue: "3.9027282148023636"},
 
-      // Testing negative integers, including large numbers
+      // Testing negative integers, including large numbers and floats
       {numerator: "-6", denominator: "-2", expectedValue: "3"},
       {numerator: "-10", denominator: "-5", expectedValue: "2"},
       {numerator: "-150", denominator: "-3", expectedValue: "50"},
+      {numerator: "-1.5", denominator: "-3", expectedValue: "0.5"},
+      {numerator: "-15045698.215478", denominator: "-15.9658", expectedValue: "942370.4553156121"},
       {numerator: "-1500000000000000", denominator: "-399999999999999", expectedValue: "3.7500000000000093"},
       {numerator: "-15597562143664", denominator: "-3996579132645", expectedValue: "3.9027282148023636"},
 
-      // Testing dividing negative integers by positive integers, including large numbers
+      // Testing dividing negative integers by positive integers, including large numbers and floats
       {numerator: "-6", denominator: "2", expectedValue: "-3"},
       {numerator: "-10", denominator: "5", expectedValue: "-2"},
       {numerator: "-150", denominator: "3", expectedValue: "-50"},
+      {numerator: "-1.5", denominator: "3", expectedValue: "-0.5"},
+      {numerator: "-15045698.215478", denominator: "15.9658", expectedValue: "-942370.4553156121"},
       {numerator: "-1500000000000000", denominator: "399999999999999", expectedValue: "-3.7500000000000093"},
       {numerator: "-15597562143664", denominator: "3996579132645", expectedValue: "-3.9027282148023636"},
 
-      // Testing dividing positive integers by negative integers, including large numbers
+      // Testing dividing positive integers by negative integers, including large numbers and floats
       {numerator: "6", denominator: "-2", expectedValue: "-3"},
       {numerator: "10", denominator: "-5", expectedValue: "-2"},
       {numerator: "150", denominator: "-3", expectedValue: "-50"},
+      {numerator: "1.5", denominator: "-3", expectedValue: "-0.5"},
+      {numerator: "15045698.215478", denominator: "-15.9658", expectedValue: "-942370.4553156121"},
       {numerator: "1500000000000000", denominator: "-399999999999999", expectedValue: "-3.7500000000000093"},
       {numerator: "15597562143664", denominator: "-3996579132645", expectedValue: "-3.9027282148023636"},
  
