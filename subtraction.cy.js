1c811261bbe49361f12b54f096430d6158e5ff3d
kilianska <151628659+kilianska@users.noreply.github.com>
2024-01-10 13:11:32 +0000

    Add files via upload

--- subtraction.cy.js
+++ subtraction.cy.js
@@ -2,7 +2,7 @@ describe('subtraction test', () => {
     it('should correctly subtract two numbers', () => {
       cy.visit('../../index.html')
       
-      const subtract = (numerator, denominator, expectedValue) => {
+      const subtract = (firstnumber, secondnumber, expectedValue) => {
            // Helper function to input a large number using buttons
           const inputLargeNumber = (largeNumber) => {
           // Split the large number into individual digits
@@ -15,9 +15,9 @@ describe('subtraction test', () => {
   
         // Testing function allows to test both negative and positive 
         // integers, including large numbers
-        inputLargeNumber(numerator);
+        inputLargeNumber(firstnumber);
         cy.contains("button", "-").click();
-        inputLargeNumber(denominator);
+        inputLargeNumber(secondnumber);
         cy.contains("button", "=").click();
         cy.get("input#display").should("have.value", expectedValue);
         cy.contains("button", "C").click();
@@ -28,24 +28,24 @@ describe('subtraction test', () => {
       
       const testCases= [
         // Testing positive integers
-        {numerator: "6", denominator: "2", expectedValue: "4"},
-        {numerator: "10", denominator: "5", expectedValue: "5"},
-        {numerator: "150", denominator: "3", expectedValue: "147"},
-        {numerator: "6000000", denominator: "4000000", expectedValue: "2000000"},
-        {numerator: "6.123456", denominator: "4.123456", expectedValue: "2" },
+        {firstnumber: "6", secondnumber: "2", expectedValue: "4"},
+        {firstnumber: "10", secondnumber: "5", expectedValue: "5"},
+        {firstnumber: "150", secondnumber: "3", expectedValue: "147"},
+        {firstnumber: "6000000", secondnumber: "4000000", expectedValue: "2000000"},
+        {firstnumber: "6.123456", secondnumber: "4.123456", expectedValue: "2" },
         
         
    // Testing negative integers
-      {numerator: "-6", denominator: "2", expectedValue: "-8"},
-      {numerator: "-10", denominator: "5", expectedValue: "-15"},
-      {numerator: "-150", denominator: "3", expectedValue: "-153"},
-      {numerator: "-6000000", denominator: "400000", expectedValue: "-6400000"},
-      {numerator: "-6.123456", denominator: "4.123456", expectedValue: "-10.246912"},
+      {firstnumber: "-6", secondnumber: "2", expectedValue: "-8"},
+      {firstnumber: "-10", secondnumber: "5", expectedValue: "-15"},
+      {firstnumber: "-150", secondnumber: "3", expectedValue: "-153"},
+      {firstnumber: "-6000000", secondnumber: "400000", expectedValue: "-6400000"},
+      {firstnumber: "-6.123456", secondnumber: "4.123456", expectedValue: "-10.246912"},
 
       ]
      
-    testCases.forEach(({ numerator, denominator, expectedValue }) => {
-        subtract(numerator, denominator, expectedValue);
+    testCases.forEach(({ firstnumber, secondnumber, expectedValue }) => {
+        subtract(firstnumber, secondnumber, expectedValue);
       });
     });
 })
